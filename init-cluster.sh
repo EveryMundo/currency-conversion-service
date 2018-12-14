8ec267df2b4e64e6c6b888d78fa72dc67b78f5ce
Daniel Santana <github@danielsantana.net>
2018-12-14 13:45:14 -0500

    makes init-cluster.sh to load ENV VARS from .env if it exists

--- init-cluster.sh
+++ init-cluster.sh
@@ -2,6 +2,22 @@
 source $HOME/.nvm/nvm.sh
 nvm use stable
 
+set -e
+
+if [ -f .env ]; then
+  for line in $(grep -P '^\w+' .env); do
+    varName=$(echo "$line" | grep -Po '^\w+')
+    eval "TEMPV=\$${varName}"
+
+    if [ "$TEMPV" = "" ]; then
+      echo "eval export $line"
+      eval "export $line"
+    fi;
+  done
+else
+  echo "no .env file found :D"
+fi
+
 cd $(dirname $0)
 
 node ./cluster.js
